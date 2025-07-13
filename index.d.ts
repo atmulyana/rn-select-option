5ec5c8903d6841e0d9001ebfb7ffa74a63b89a62
AT Mulyana <atmulyana@yahoo.com>
2025-07-13 21:05:48 +0700

    v0.0.7
    updates declaration files (Typescript and Flow)

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,16 @@
 /**
  * https://github.com/atmulyana/rn-select-option
  */
-import type {Component} from 'react';
 import {Picker} from '@react-native-picker/picker';
-import type {PickerProps, PickerItemProps} from '@react-native-picker/picker';
+import type {PickerItemProps} from '@react-native-picker/picker';
 import type {ItemValue} from '@react-native-picker/picker/typings/Picker';
 
-declare class _Select<T> extends Picker<T>
-    implements Component<PickerProps<T> & {
-        placeholder?: string,
-        placeholderTextColor?: PickerProps<T>['dropdownIconColor'],
-    }>
-{}
-declare class Select extends _Select<ItemValue> {}
-declare function select<T>(): _Select<T>;
+declare type SelectType<T = ItemValue> = typeof Picker<T>;
+declare var Select: SelectType;
+declare function select<T>(): SelectType<T>;
 
-declare type OptionType<T> = React.ComponentType<PickerItemProps<T>>;
-declare var Option: OptionType<ItemValue>;
+declare type OptionType<T = ItemValue> = React.ComponentType<PickerItemProps<T>>;
+declare var Option: OptionType;
 declare function option<T>(): OptionType<T>;
 
 export {Select, select, Option, option};
